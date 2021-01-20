63a09b3d1e3a33ceaddfe6dc558f5aa257f66ca3
Ken Zou <ken19970723@gmail.com>
2021-01-20 12:39:35 +0800

    Update 2021-01-20T04:39:28.951Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 console.log('Hello!');
 $(document).ready(function () {
   //swiper
@@ -54,27 +56,93 @@ $(document).ready(function () {
     autoplay: {
       delay: 3000
     }
-  }); // productDetail reserve input
+  }); // destination
+
+  var zones = document.querySelectorAll('.zone li');
+  var zoneInput = document.querySelector('.zoneInput');
+  console.log(zoneInput);
+  zones.forEach(function (zone, i) {
+    zoneInput.value = '';
+    zones[i].addEventListener('click', function () {
+      zoneInput.classList.remove('d-none');
+
+      if (zone == zones[0]) {
+        zoneInput.value = 'Bangkok，Thailand';
+      } else if (zone == zones[1]) {
+        zoneInput.value = 'Osaka，Japan';
+      } else if (zone == zones[2]) {
+        zoneInput.value = 'Roma, Italy';
+      } else if (zone == zones[3]) {
+        zoneInput.value = 'Paris, France';
+      } else if (zone == zones[4]) {
+        zoneInput.value = 'Reykjavik, iceland';
+      }
+    });
+  }); // index person
+
+  var personQuantity = document.querySelectorAll('.personQuantity');
+  var addPerson = document.querySelectorAll('.addPerson');
+  var minusPerson = document.querySelectorAll('.minusPerson');
+  var count = document.querySelector('.count');
+  var change = document.querySelectorAll('.changeQty');
+  var quantity = '';
+
+  var _loop = function _loop(i) {
+    addPerson[i].addEventListener('click', function () {
+      count.classList.remove('d-none');
+      personQuantity[i].value++;
+    });
+    minusPerson[i].addEventListener('click', function () {
+      if (personQuantity[i].value <= 0) {
+        personQuantity[i].value = 0;
+        minusPerson[i].classList.add('disabled');
+      } else {
+        personQuantity[i].value--;
+      }
+    });
+  };
+
+  for (var i = 0; i < addPerson.length; i++) {
+    _loop(i);
+  }
+
+  for (var _i = 0; _i < change.length; _i++) {
+    change[_i].addEventListener('click', function () {
+      if (personQuantity[0].value == 0 && personQuantity[1].value > 0) {
+        quantity = "".concat(personQuantity[1].value, " child\u3001 ").concat(personQuantity[2].value, " room");
+      } else if (personQuantity[1].value == 0 && personQuantity[0].value > 0) {
+        quantity = "".concat(personQuantity[0].value, " adult\u3001 ").concat(personQuantity[2].value, " room");
+      } else if (personQuantity[0].value == 0 && personQuantity[1].value == 0) {
+        quantity = "".concat(personQuantity[2].value, " room");
+      } else {
+        quantity = "".concat(personQuantity[0].value, " adult\u3001").concat(personQuantity[1].value, " child\u3001").concat(personQuantity[2].value, " room");
+      }
+
+      count.value = quantity;
+    });
+  } // productDetail reserve input
+
 
   var roomQuantity = document.querySelectorAll('.roomQuantity');
   var addRoom = document.querySelectorAll('.addRoom');
   var minusRoom = document.querySelectorAll('.minusRoom');
 
-  var _loop = function _loop(i) {
-    addRoom[i].addEventListener('click', function () {
-      roomQuantity[i].value++;
+  var _loop2 = function _loop2(_i2) {
+    addRoom[_i2].addEventListener('click', function () {
+      roomQuantity[_i2].value++;
     });
-    minusRoom[i].addEventListener('click', function () {
-      if (roomQuantity[i].value <= 0) {
-        minusRoom[i].classList.add('disabled');
+
+    minusRoom[_i2].addEventListener('click', function () {
+      if (roomQuantity[_i2].value <= 0) {
+        minusRoom[_i2].classList.add('disabled');
       } else {
-        roomQuantity[i].value--;
+        roomQuantity[_i2].value--;
       }
     });
   };
 
-  for (var i = 0; i < addRoom.length; i++) {
-    _loop(i);
+  for (var _i2 = 0; _i2 < addRoom.length; _i2++) {
+    _loop2(_i2);
   } // alert animation
 
 
@@ -86,17 +154,26 @@ $(document).ready(function () {
   }); // daterangepicker
 
   $(function () {
+    var _locale;
+
     $('#picker').daterangepicker({
       autoUpdateInput: false,
-      locale: {
-        cancelLabel: 'Clear'
-      }
+      locale: (_locale = {
+        cancelLabel: 'Clear',
+        daysOfWeek: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+        format: "YYYY-MM-DD HH:mm:ss",
+        //設定顯示格式
+        applyLabel: '確定'
+      }, _defineProperty(_locale, "cancelLabel", '取消'), _defineProperty(_locale, "customRangeLabel", '自定義'), _locale)
     });
-    $('#picker').on('apply.daterangepicker', function (ev, picker) {
-      $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+    $('#picker').click(function (e) {
+      e.preventDefault();
     });
-    $('#picker').on('cancel.daterangepicker', function (ev, picker) {
-      $(this).val('');
+    $('#picker').on('apply.daterangepicker', function (ev, picker, e) {
+      $('input[name="datefilter"]').val("".concat(picker.startDate.format('YYYY-MM-DD'), " - ").concat(picker.endDate.format('YYYY-MM-DD')));
+      $('.userDate').removeClass('d-none');
+      $('.defaultText').addClass('d-none');
+      $('.defaultText').removeClass('d-flex');
     });
   });
   $('#person_dropdown').on("click", function (e) {
